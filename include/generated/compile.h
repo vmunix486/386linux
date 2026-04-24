8af7bd8827c7d034630e95cd7a476735ffa0771a
Marco Esquandolas <justaguy8086@gmail.com>
2026-04-24 19:33:48 -0400

    Commit via CLI

---
+++ include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 7 */
+#define UTS_MACHINE "i386"
+#define UTS_VERSION "#7 Fri Apr 24 17:18:58 EDT 2026"
+#define LINUX_COMPILE_BY "vmunix"
+#define LINUX_COMPILE_HOST "heliosphan"
+#define LINUX_COMPILER "gcc version 15.2.0 (T2 SDE) "
